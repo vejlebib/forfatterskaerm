dfedd324619114124bc4d3136ab68d7b2e112789
holt83 <simoh@vejlebib.dk>
2018-08-28 17:42:06 +0200

    Fix some issues with routing

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ var author_list = author_list_tmpl({authors: authors.authors});
 
 var video_tmpl = require('views/video');
 
-var router = new (require('navigo'))();
+var router = new (require('navigo'))(getBaseURL());
 const author_video = require('author_video');
 
 // Setup some simple routing in the client with javascript.
@@ -16,10 +16,10 @@ router.on({
 
     $('ul#author-list a').click(function(e) {
       e.preventDefault();
-      router.navigate('/forfattere/' + $(this).data('name'));
+      router.navigate(getBaseURL() + '/forfatter/' + $(this).data('name'), true);
     });
   },
-  '/forfattere/:id': function(params) {
+  '/forfatter/:id': function(params) {
     $('#main-view').html(video_tmpl({author: params.id}));
 
     $('#close-video').on('click touch', function(e) {
@@ -30,3 +30,7 @@ router.on({
     author_video.startAuthorVideo(params.id);
   }
 }).resolve();
+
+function getBaseURL() {
+  return window.location.protocol + '//' + window.location.hostname;
+}
